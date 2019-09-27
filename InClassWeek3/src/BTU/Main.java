6420a4f0a840ec71614bac2c83ca777884da27fb
student <sandro.melia.1@btu.eu>
2019-09-27 10:08:53 +0400

    Week 3 Project

---
+++ InClassWeek3/src/BTU/Main.java
@@ -0,0 +1,11 @@
+package BTU;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Java");
+        int x = 12;
+        System.out.println(x+100);
+    }
+}
